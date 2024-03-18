a8a247d372ef8d77e4746131ab5107b747f4a339
jda0043 <90729752+jda0043@users.noreply.github.com>
2024-03-18 12:49:02 -0500

    Add files via upload

---
+++ joycon_controller.py
@@ -0,0 +1,192 @@
+from pyjoycon import JoyCon, get_L_id
+import time
+import asyncio
+from websockets.sync.client import connect
+from os import linesep
+
+
+
+host = "ws://192.168.4.1/ws"
+
+time.sleep(2)
+global websocket
+websocket = connect(host)
+print("Connection Started")
+time.sleep(2)
+
+MAX_ANGLE = 20
+MIN_ANGLE = -20
+
+deadband = 5
+global previous_time
+
+joycon_id = get_L_id()
+joycon = JoyCon(*joycon_id)
+status = joycon.get_status()
+
+sticks = status.get('analog-sticks')
+leftStick = sticks.get('left')
+horizontal_stick = leftStick.get('horizontal')
+vertical_stick = leftStick.get('vertical')
+
+buttons = status.get('buttons')
+left_buttons = buttons.get('left')
+d = left_buttons.get('down')
+u = left_buttons.get('up')
+l = left_buttons.get('left')
+r = left_buttons.get('right')
+sl = left_buttons.get('sl')
+sr = left_buttons.get('sr')
+ll = left_buttons.get('l')
+z = left_buttons.get('zl')
+
+global d_p,u_p,l_p,r_p,sl_p,sr_p,ll_p,z_p
+d_p = d
+u_p = u
+l_p = l
+r_p = r
+sl_p = sl
+sr_p = sr 
+ll_p = ll
+z_p = z
+
+global pitch_offset
+pitch_offset = 0
+
+
+pitch_setpoint = 0
+heading_setpoint = 0
+
+
+def map_range(x, in_min, in_max, out_min, out_max):
+  return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+def update_stick_pos():
+    status = joycon.get_status()
+    sticks = status.get('analog-sticks')
+    leftStick = sticks.get('left')
+    global horizontal_stick
+    global vertical_stick
+    horizontal_stick = leftStick.get('horizontal')
+    vertical_stick = leftStick.get('vertical')
+    buttons = status.get('buttons')
+    left_buttons = buttons.get('left')
+    global d,u,l,r,sl,sr,ll,z
+    d = left_buttons.get('down')
+    u = left_buttons.get('up')
+    l = left_buttons.get('left')
+    r = left_buttons.get('right')
+    sl = left_buttons.get('sl')
+    sr = left_buttons.get('sr')
+    ll = left_buttons.get('l')
+    z = left_buttons.get('zl')
+    
+
+    
+    
+
+previous_time = time.time()
+while (True):
+    current_time = time.time() 
+    delta_time = current_time - previous_time
+    update_stick_pos()
+    
+    if (d != d_p):
+        print("D: ",d)
+        if (d == 1):
+            pitch_offset = pitch_offset + 0.25
+            if (pitch_offset > 15):
+                pitch_offset = 15
+            # Websocket Message
+            websocket.send("4s" + str(pitch_offset) )
+            print("Pitch Offset: ",pitch_offset)
+        d_p = d
+    if (u != u_p):
+        print("U: ",u)
+        if (u == 1):
+            pitch_offset = pitch_offset - 0.25
+            if (pitch_offset < -15):
+                pitch_offset = -15
+            # Websocket Message
+            websocket.send("4s" + str(pitch_offset) )
+            print("Pitch Offset: ",pitch_offset)
+        u_p = u
+    if (l != l_p):
+        print("L: ",l)
+        if (l == 1):
+            MAX_ANGLE = MAX_ANGLE - 1
+            MIN_ANGLE = MIN_ANGLE + 1
+            if (MAX_ANGLE < 10):
+                MAX_ANGLE = 10
+            if (MIN_ANGLE > -10):
+                MIN_ANGLE = -10
+            print("Max Angle: ",MAX_ANGLE)
+            print("Min Angle: ",MIN_ANGLE)
+        l_p = l
+    if (r != r_p):
+        print("R: ",r)
+        if (r == 1):
+            MAX_ANGLE = MAX_ANGLE + 1
+            MIN_ANGLE = MIN_ANGLE - 1
+            if (MAX_ANGLE > 50):
+                MAX_ANGLE = 50
+            if (MIN_ANGLE < -50):
+                MIN_ANGLE = -50
+            print("Max Angle: ",MAX_ANGLE)
+            print("Min Angle: ",MIN_ANGLE)
+        r_p = r
+    if (sl != sl_p):
+        print("SL: ",sl)
+        if (sl == 1):
+            deadband = deadband - 1
+            if (deadband < 0):
+                deadband = 0
+            print("Deadband: ",deadband)
+        sl_p = sl
+    if (sr != sr_p):
+        print("SR: ",sr)
+        if (sr == 1):
+            deadband = deadband + 1
+            if (deadband > 15):
+                deadband = 15
+            print("Deadband: ",deadband)
+        sr_p = sr
+        
+    if (ll != ll_p):
+        print("LL: ",ll)
+        if (ll == 1):
+            # Websocket Message
+            websocket.send("ENMOT")
+            print("Motors ENABLED")
+        ll_p = ll
+    if (z != z_p):
+        print("Z: ",z)
+        if (z == 1):
+            # Websocket Message
+            websocket.send("DISMOT")
+            print("Motors DISABLED")
+        z_p = z
+
+    
+    pitch_setpoint = map_range(horizontal_stick,750,3125,MIN_ANGLE,MAX_ANGLE)
+    heading_setpoint = map_range(vertical_stick,3380,1250,MIN_ANGLE,MAX_ANGLE)
+    
+    if (pitch_setpoint <= deadband and pitch_setpoint > 0):
+        pitch_setpoint = 0
+    if (heading_setpoint <= deadband and heading_setpoint > 0):
+        heading_setpoint = 0
+    if (pitch_setpoint >= -deadband and pitch_setpoint < 0):
+        pitch_setpoint = 0
+    if (heading_setpoint >= -deadband and heading_setpoint < 0):
+        heading_setpoint = 0
+    
+
+    if (delta_time >= 0.075):
+        print("Pitch Setpoing: ", pitch_setpoint,"      Heading Setpoing: ", heading_setpoint)
+        # Websocket Message
+        websocket.send("SP" + str(pitch_setpoint) + "," + str(heading_setpoint) + ",")
+        previous_time = current_time
+    
+    
+    
+   
